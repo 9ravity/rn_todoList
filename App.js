94d43b8f8fcd4af9a6b75cecbc323b63bcc0f302
9ravity <shegoback@naver.com>
2020-02-17 14:28:07 +0900

    style

--- App.js
+++ App.js
@@ -1,14 +1,57 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  Dimensions,
+  Platform
+} from "react-native";
+
+const { width, height } = Dimensions.get("window");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>todoList</Text>
+      <StatusBar barStyle="light-content"></StatusBar>
+      <Text style={styles.title}>todoList</Text>
+      <View style={styles.listContent}></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {}
+  container: {
+    flex: 1,
+    backgroundColor: "#f23657",
+    alignItems: "center"
+  },
+  title: {
+    color: "white",
+    fontSize: 30,
+    marginTop: 50,
+    marginBottom: 30,
+    fontWeight: "200"
+  },
+  listContent: {
+    backgroundColor: "white",
+    flex: 1,
+    width: width - 25,
+    borderRadius: 10,
+    marginBottom: 20,
+    ...Platform.select({
+      ios: {
+        shadowColor: "rgb(50,50,50)",
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: -1,
+          width: 0
+        }
+      },
+      android: {
+        elevation: 10
+      }
+    })
+  }
 });
